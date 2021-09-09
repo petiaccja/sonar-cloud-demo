7cf3d50e100306f004ab685b5ba919b8b501093a
Peter Kardos <kardospeter1994@hotmail.com>
2021-09-09 16:16:13 +0200

    more natural bugs

--- SonarCloudDemo.cpp
+++ SonarCloudDemo.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <iostream>
 
-struct Position {
+struct Point {
 	float x, y;
 };
 
@@ -13,30 +13,44 @@ struct Health {
 class Player {
 public:
 	Player() = default;
-	Player(Position position) : Player(position, { 0, 100 }) {}
-	Player(Health health) : Player({ 0,0 }, health) {}
-	Player(Position position, Health health) { position = position; health = health; }
+	Player(Point position, Health health) : position(position), health(health) {}
 
+	// Health
 	int GetCurrentHealth() { return health.current; }
-	int SetCurrentHealth(int currentHealth) { return health.current = std::min(currentHealth, health.maximum); }
+	int SetCurrentHealth(int currentHealth) { return health.current = std::min(std::max(0, currentHealth), health.maximum); }
 
 	int GetMaximumHealth() { return health.current; }
+
+	// Position
+	Point GetPosition() const { return position; }
+	void SetPosition(Point position) const { position = position; }
+
 protected:
-	Position position;
+	Point position;
 	Health health;
 };
 
-void Damage(Player& player) {
-	const int currentHealth = player.GetCurrentHealth();
-	const int maxmimumHealth = player.GetMaximumHealth();
-	const double fraction = currentHealth < maxmimumHealth / 2 ? (1 / 2) : (1 / 3); //  Less damage on low health.
-	player.SetCurrentHealth(int(currentHealth * fraction));
+void Damage(Player& player, int distance) {
+	if (distance = 0) {
+		player.SetCurrentHealth(0);
+	}
+	else {
+		const int health = player.GetCurrentHealth();
+		const int damage = health / distance;
+		player.SetCurrentHealth(health - damage);
+	}
+}
+
+void Move(Player& player, int stepX, int stepY) {
+	Point pos = player.GetPosition();
+	pos.x += stepX;
+	pos.x += stepY;
+	player.SetPosition(pos);
 }
 
 int main() {
 	Player player{ { 2, 7 }, { 100, 100 } };
-	std::cout << player.GetCurrentHealth() << std::endl;
-	Damage(player);
-	std::cout << player.GetCurrentHealth() << std::endl;
+	Damage(player, 3);
+
 	return 0;
 }
